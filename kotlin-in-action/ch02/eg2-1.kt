8aa879687b1c2d031a9b1b9c33bf665bb3e7fe50
Kwanghyun Kim <32362748+nokchax@users.noreply.github.com>
2021-08-26 23:32:53 +0900

    Delete eg2-1.kt

--- kotlin-in-action/ch02/eg2-1.kt
+++
@@ -1,9 +0,0 @@
-package ch02
-
-fun main(args: Array<String>) { // args 는 생략이 가능
-    println("Hello, world!")
-}
-
-// 함수 선언시 fun 키워드 사용
-// 파라미터 이름 뒤에 타입을 쓴다
-// 함수를 최상위 수준에 정의할 수 있다. 자바와 달리 클래스 안에 넣어야 할 필요 없음
